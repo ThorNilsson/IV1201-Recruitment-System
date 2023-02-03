e18223099cb8cfc109bea2c91ca4867a8f393c2c
thor <thor756nilsson@gmail.com>
2023-02-03 16:49:13 +0100

    Admin page list

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 import { authRouter } from "./routers/auth";
+import { adminRouter } from "./routers/admin";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { authRouter } from "./routers/auth";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   auth: authRouter,
+  admin: adminRouter,
 });
 
 // export type definition of API
